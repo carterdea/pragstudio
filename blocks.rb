2c71f2092335bcf0d69495a28517be14eea8aa0f
Carter De Angelis <me@carterdea.com>
2015-01-26 15:17:25 -0800

    testing out blocks and sorting

--- blocks.rb
+++ blocks.rb
@@ -1,7 +1,7 @@
-3.times { puts "Echo"}
+3.times { puts 'Echo' }
 
 1.upto(10) do |number|
-	puts "#{number} situp"
-	puts "#{number} pushup"
-	puts "#{number} chinup"
+  puts "#{number} situp"
+  puts "#{number} pushup"
+  puts "#{number} chinup"
 end
