daf2c43a304df5d2dca8c120a4018721cfdae736
Carter De Angelis <me@carterdea.com>
2015-05-23 15:39:44 -0700

    Retaking course from scratch :)

--- blocks.rb
+++
@@ -1,7 +0,0 @@
-3.times { puts 'Echo' }
-
-1.upto(10) do |number|
-  puts "#{number} situp"
-  puts "#{number} pushup"
-  puts "#{number} chinup"
-end
--- flicks/conditionals.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'movie'
-
-movie = Movie.new('godfather', 10)
-
-if movie.rank >= 10
-  puts 'Hit'
-else
-  puts 'Flop'
-end
